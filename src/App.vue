a84d2b53ee8c36688f1d75c64e9c15fb8a970a66
Gabriel Giarratana <gabriel.giarratana@gmail.com>
2020-10-29 15:21:26 -0500

    Basic template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <template>
   <v-app>
+    <v-col>
+      <v-row>
+        <v-btn
+          elevation="2"
+        >Fade LEDs</v-btn>
+        <v-color-picker
+          itemid="left-color"
+          dot-size="25"
+          swatches-max-height="20"
+          elevation="2"
+        >
+        </v-color-picker>
+        <v-color-picker
+          itemid="right-color"
+          dot-size="25"
+          swatches-max-height="20"
+          elevation="2"
+        >
+        </v-color-picker>
+      </v-row>
+      <v-divider></v-divider>
+    </v-col>
   </v-app>
 </template>
 
